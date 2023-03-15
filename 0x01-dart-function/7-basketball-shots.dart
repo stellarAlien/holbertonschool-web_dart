9daac07e3779b9222ff092ab6ba304b6132709fe
stellarAlien <saifgaida@gmail.com>
2023-03-15 09:02:33 +0100

    null operator

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,6 +1,6 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  int teamAPoints = teamA['Free throws']! + teamA['2 pointer']! * 2 + teamA['3 pointer']! * 3;
-  int teamBPoints = teamB['Free throws']! + teamB['2 pointer']! * 2 + teamB['3 pointer']! * 3;
+  int teamAPoints = (teamA['Free throws'] ?? 0) + (teamA['2 pointer'] ?? 0) * 2 + (teamA['3 pointer'] ?? 0) * 3;
+  int teamBPoints = (teamB['Free throws'] ?? 0) + (teamB['2 pointer'] ?? 0) * 2 + (teamB['3 pointer'] ?? 0) * 3;
 
   if (teamAPoints > teamBPoints) {
     return 1;
