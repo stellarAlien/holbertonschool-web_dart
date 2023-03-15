28dff615a455a84d092ea953a5c2c3b36cdd0808
stellarAlien <saifgaida@gmail.com>
2023-03-15 08:29:02 +0100

    fixed output

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,7 @@
 void outer(String name, String id) {
     String inner() {
-        print("Hello Agent ${name} your id is ${id}");
+        String last_name = name.split(' ')[1][0] + '.';
+        print("Hello Agent ${last_name}${name} your id is ${id}");
         return '';
     }
     inner();
