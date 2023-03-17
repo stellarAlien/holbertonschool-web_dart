f8f2a1a145dc4efffa349de7970e43324693e823
stellarAlien <saifgaida@gmail.com>
2023-03-17 13:52:02 +0100

    fixed output again

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -24,6 +24,6 @@ class User {
   }
 
   String toString() {
-    return 'User(id : $id, name: $name, age: $age, height: $height)';
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
   }
 }
