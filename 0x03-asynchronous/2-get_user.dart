d0a1a244517a340ee75c8f12f970f1864424ec57
stellarAlien <saifgaida@gmail.com>
2023-03-21 20:32:27 +0100

    util

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,10 +1,4 @@
-Future<String> fetchUser() =>
-    // Imagine that this function is
-    // more complex and slow.
-    Future.delayed(
-      const Duration(seconds: 2),
-      () => throw 'Cannot locate user',
-    );
+import '2-util.dart';
 
 Future<void> getUser() async {
   try {
