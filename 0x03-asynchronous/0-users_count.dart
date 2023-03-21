03772657f872850a39992c1499ff8fad5ca1897d
stellarAlien <saifgaida@gmail.com>
2023-03-21 20:27:33 +0100

    util

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,7 +1,4 @@
-Future<int> fetchUsersCount() => Future.delayed(
-      const Duration(seconds: 2),
-      () => 19,
-    );
+import '0-util.dart';
 Future<void> usersCount() async {
   int count = await fetchUsersCount();
   print(count);
