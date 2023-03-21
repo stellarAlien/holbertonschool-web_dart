2892ebc0f9f6792e26a62de3bd36059b4f09cdc5
stellarAlien <saifgaida@gmail.com>
2023-03-21 20:33:44 +0100

    utils?

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,6 +1,23 @@
 import 'dart:convert';
 import '3-util.dart';
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
 
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
+
+Future<String> greetUser() async {
+  try {
+    final userData = await fetchUserData();
+    final decodedData = json.decode(userData);
+    return 'Hello ${decodedData['username']}';
+  } catch (error) {
+    return 'error caught: $error';
+  }
+}
 
 Future<String> loginUser() async {
   try {
